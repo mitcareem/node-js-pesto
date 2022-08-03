022295269c6ff4714ef61676c19154e01a9c9706
mitcareem <102579201+mitcareem@users.noreply.github.com>
2022-08-03 21:02:29 +0530

    Update index.js
    vv

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-console.log("hello world");
-function app() {
-    return "this is app function";
-}
+const express = require("express");
+const superheroes = require('superheroes');
+
+var name = superheroes.random();
+console.log(name);
+
+const app = express();
+
+app.get("/name", function (req, res) {
+    res.send("hello from express");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log("server is started");
+});
