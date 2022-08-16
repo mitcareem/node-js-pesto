6eb7c81a4e4f9b6256e28b7c3d3825236f2d5dce
mitcareem <mitcareem@gmail.com>
2022-08-16 22:20:51 +0530

    port added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
-const PORT = 3000 || process.env.PORT;
+
 
 app.get('/api/v1/:token', (req, res) => {
 
     res.status(200).json({ name: req.params.token })
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
